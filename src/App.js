a23962ae980917c9cfa337aa1100319be50e8385
hadis <hadisghafouri98@gmail.com>
2021-02-22 17:56:57 +0330

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Component } from 'react';
+import {CardList} from './component/card-list/card-list.component'
+import { SearchBox } from "./component/card-list/search-box/search-box.component";
+class App extends Component{
+  constructor(){
+    super();
+
+
+    this.state = {
+     monsters:[],
+     searchFiled:''
+     
+    };
+    // this.handleChange = this.handleChange.bind(this);
+  }
+//arwo function bind automatically 
+  handleChange = (e)=>{
+    this.setState({searchFiled:e.target.value});
+  }
+  // handleChange(e){
+  //   this.setState({searchFiled:e.target.value});
+  // }
+  componentDidMount(){
+
+    fetch('https://jsonplaceholder.typicode.com/users').then(response =>response.json())
+    .then(users => this.setState({monsters:users}));
+    
+  }
+
+  render(){
+    const {monsters, searchFiled} = this.state;//= const monsters = this.state.monsters;
+    //const searchFiled = this.state.searchFiled;
+    const filteredMonsters = monsters.filter(monster =>monster.name.toLowerCase().includes(searchFiled.toLocaleLowerCase()))
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder="search monsters" 
+        handleChange = {this.handleChange}
+        // handleChange={e=>this.setState({searchFiled:e.target.value})}
+        
+        
+        />
+        
+        <CardList monsters={filteredMonsters}/>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
